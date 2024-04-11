38402ba1a08eca92e1dcdb04b235d44139b0ff95
MaciejBodnar <maciej.bodnar2@gmail.com>
2024-04-11 12:27:01 +0200

    fix bug with total m2

--- main.js
+++ main.js
@@ -2,19 +2,21 @@ import { placeholderPropertyObj } from "./properties/placeholderPropertyObj.js";
 import { propertyForSaleArr } from "./properties/propertyForSaleArr.js";
 
 const getPropertyHtml = (saleArr = placeholderPropertyObj) => {
-  const popertyTotalSize = saleArr.reduce((total, currentItem) => {
-    return total + currentItem.roomsM2[0];
-  }, 0);
+  // prettier-ignore
+  const propertyArr = saleArr.map((prop) => {
+      const { propertyLocation, priceGBP, roomsM2, comment, image } = prop;
 
-  const propertyArr = saleArr
-    .map((prop) => {
+      const totalRoomSize = roomsM2.reduce((total, currentItem) => {
+        return total + currentItem;
+      });
+
       return (prop.innerHTML = `<section class="card">
-                            <img src="./images/${prop.image}">
+                            <img src="./images/${image}">
                             <div class="card-right">
-                                <h2>${prop.propertyLocation}</h2>
-                                <h3>${prop.priceGBP}</h3>
-                                <p>${prop.comment}</p>
-                                <h3>${popertyTotalSize}m2</h3>
+                                <h2>${propertyLocation}</h2>
+                                <h3>${priceGBP}</h3>
+                                <p>${comment}</p>
+                                <h3>${totalRoomSize}m2</h3>
                             </div>
                         </section> 
     `);
